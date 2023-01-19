6cfb2f1e72fce87682332fa489739e485e30c6be
yifan <34758569+refact0r@users.noreply.github.com>
2023-01-19 20:23:25 +0000

    cat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
+	import catgif from '$lib/images/cat.gif';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>hello</title>
 	<meta name="description" content="description" />
 </svelte:head>
 
 <section>
 	<h1>hello world</h1>
+	<img src={catgif} alt="cat gif" />
 </section>
 
 <style>
